4f33f66193e8ebd37601c3046cad007599b48c4a
RobertS <rshugart@protonmail.com>
2019-02-14 13:29:36 -0500

    Create VideoItem and imported it into VideoList

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import VideoItem from "../components/VideoItem";
 
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+//Destructured props by usin ({ }) and putting videos inside
+const VideoList = ({ videos }) => {
+  videos.map(video => {
+    return <VideoItem />;
+  });
+  return <div>{videos.length}</div>;
 };
 
 export default VideoList;
