5fb5a7ae3019181c3a9712c331a2acbc539f3b63
RobertS <rshugart@protonmail.com>
2019-02-14 12:14:27 -0500

    Created and configured axios export

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDm6Gi6l4SsmBzQBDSjT8Aqrniqhwd08JE";
+
+export default axios.create({
+  baseURL: "https://www.googeapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
