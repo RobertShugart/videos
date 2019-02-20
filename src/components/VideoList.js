3535c0465304f7715ff123254bd9f72fb8fb345f
RobertS <rshugart@protonmail.com>
2019-02-20 13:16:07 -0500

    Added a key to VideoDetail,

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,13 @@ import VideoItem from "../components/VideoItem";
 //Destructured props by usin ({ }) and putting videos inside
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
